72027cf390882156c3e6f9c8287362f619bf49e4
William Collins <wcurtiscollins@gmail.com>
2016-12-08 11:45:08 -0500

    add download page

--- app/controllers/games/tiny_controller.rb
+++ app/controllers/games/tiny_controller.rb
@@ -1,6 +1,13 @@
 class Games::TinyController < ApplicationController
 
   def index
+    builds = Build.where(game: 'tiny')
+
+    @latest = {
+      x32: builds.where(platform: 'tiny-x32').last,
+      x64: builds.where(platform: 'tiny-x64').last,
+      osx: builds.where(platform: 'tiny-osx').last
+    }
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def aws_url relative
+    bucket = Rails.application.secrets[:aws]["bucket"]
+    "http://s3.amazonaws.com/#{bucket}/#{relative}"
+  end
+
 end
