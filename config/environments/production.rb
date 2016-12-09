bd86d40ff73e9ceada2f6ecc5867dab7a8417726
William Collins <wcurtiscollins@gmail.com>
2016-12-08 22:04:36 -0500

    setyuup mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,16 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: Rails.application.secrets[:smtp]['host'],
+    port: 587,
+    user_name: Rails.application.secrets[:smtp]['user_name'],
+    password: Rails.application.secrets[:smtp]['password'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   Rails.application.config.middleware.use ExceptionNotification::Rack,
   :email => {
     :email_prefix => "[willyc_exception]",
